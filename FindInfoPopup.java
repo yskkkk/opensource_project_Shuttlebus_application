a8b2fb79ae5f8b0d1dd52c679e06689e527de877
ChoiYoungHan <102001212+ChoiYoungHan@users.noreply.github.com>
2022-06-12 13:23:10 +0900

    Create FindInfoPopup.java

---
+++ FindInfoPopup.java
@@ -0,0 +1,14 @@
+package com.example.mainproject0606;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindInfoPopup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_info_popup);
+    }
+}
