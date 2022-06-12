d408034ff79c236beb3d9f92e657d7aceef3a781
ChoiYoungHan <102001212+ChoiYoungHan@users.noreply.github.com>
2022-06-12 13:20:22 +0900

    Create ChangeUserInfoPopup.java

---
+++ ChangeUserInfoPopup.java
@@ -0,0 +1,14 @@
+package com.example.mainproject0606;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChangeUserInfoPopup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_user_info_popup);
+    }
+}
