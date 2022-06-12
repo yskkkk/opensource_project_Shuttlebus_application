5ea4443e62f385b64ddb1d793a05eb4972340789
ChoiYoungHan <102001212+ChoiYoungHan@users.noreply.github.com>
2022-06-12 13:21:37 +0900

    Create Comment.java

---
+++ Comment.java
@@ -0,0 +1,32 @@
+package com.example.mainproject0606;
+
+public class Comment {
+
+    private String UserNickname, UserComment, UserEmail;
+
+    public Comment() { }
+
+    public String getUserNickname() {
+        return UserNickname;
+    }
+
+    public void setUserNickname(String userNickname) {
+        UserNickname = userNickname;
+    }
+
+    public String getUserComment() {
+        return UserComment;
+    }
+
+    public void setUserComment(String userComment) {
+        UserComment = userComment;
+    }
+
+    public String getUserEmail() {
+        return UserEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        UserEmail = userEmail;
+    }
+}
