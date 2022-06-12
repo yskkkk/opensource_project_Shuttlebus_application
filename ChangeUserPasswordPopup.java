753b40a98ea095f9625c752e4f63872989d189d0
ChoiYoungHan <102001212+ChoiYoungHan@users.noreply.github.com>
2022-06-12 13:21:03 +0900

    Create ChangeUserPasswordPopup.java

---
+++ ChangeUserPasswordPopup.java
@@ -0,0 +1,14 @@
+package com.example.mainproject0606;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangeUserPasswordPopup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_user_password_popup);
+    }
+}
